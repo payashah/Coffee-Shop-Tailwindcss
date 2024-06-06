956da884eb76810199aad186d12037efbe0fd19e
unknown <paya.6225@gmail.com>
2024-06-06 21:58:23 +0330

    design header

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "brown": {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        }
+      },
+      boxShadow: {
+
+        "normal": "0px 1px 10px 0px rgba(0, 0, 0, 0.05)"
+      },
+      fontFamily: {
+        "Dana": "Dana",
+        "DanaMedium": "Dana Medium",
+        "DanaDemiBold": "Dana DemiBold",
+        "MorabbaLight": "Morabba Light",
+        "MorabbaMedium": "Morabba Medium",
+        "MorabbaBold": "Morabba Bold",
+      },
+      letterSpacing: {
+        "tightest": "-0.065em"
+      }
+    },
+  },
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *")
+      addVariant("child-hover", "& > *:hover")
+    }
+  ],
+}
+
